762f041e4e34ba46e5d575faf43a0c1fda9f46f0
ifX-Dev <ifxdev03@gmail.com>
2025-10-03 23:55:45 +0100

    Javascript Basics (Functions)

--- 14.0 Introduction to Javascript ES6/script.js
+++ 14.0 Introduction to Javascript ES6/script.js
@@ -1,3 +1,75 @@
-var myName = "Ifeoluwapo";
+// var myName = "Ifeoluwapo";
 
-alert(myName);
+// alert(myName);
+
+// var userMessage = prompt("Enter your message");
+
+// alert("You have written " + userMessage.length + " characters, you have " + (280 - userMessage.length) + " characters remaining");
+
+// alert(userMessage.slice(0, 280));
+
+// var userName = prompt("Enter your name");
+
+// capFirstLetter = userName.slice(0, 1).toUpperCase();
+
+// restOfName = userName.slice(1).toLowerCase();
+
+// alert("Hello " + capFirstLetter + restOfName);
+
+// var dogAge = prompt("Enter your dog's age");
+
+// var humanAge = (dogAge - 2) * 4 + 21;
+
+// alert("Your dog's age in human years is " + humanAge);
+
+// function getMilk(money) {
+//   console.log("leaveHouse");
+//   console.log("moveRight");
+//   console.log("moveRight");
+//   console.log("moveUp");
+//   console.log("moveUp");
+//   console.log("moveUp");
+//   console.log("moveUp");
+//   console.log("moveRight");
+//   console.log("moveRight");
+
+//   var numberOfBottles = Math.floor(money / 1.5);
+
+//   console.log("buy " + numberOfBottles + " bottles of milk");
+
+//   console.log("moveLeft");
+//   console.log("moveLeft");
+//   console.log("moveDown");
+//   console.log("moveDown");
+//   console.log("moveDown");
+//   console.log("moveDown");
+//   console.log("moveLeft");
+//   console.log("moveLeft");
+//   console.log("enterHouse");
+// }
+
+// getMilk(5);
+
+// function lifeInWeeks(age) {
+//   /************Don't change the code above************/
+
+//   //Write your code here.
+//   timeLeft = 90 - age;
+//   timeLeftDays = timeLeft * 365;
+//   timeLeftWeeks = timeLeft * 52;
+//   timeLeftMonths = timeLeft * 12;
+
+//   console.log(
+//     "You have " +
+//       timeLeftDays +
+//       " days, " +
+//       timeLeftWeeks +
+//       " weeks, and " +
+//       timeLeftMonths +
+//       " months left."
+//   );
+
+//   /*************Don't change the code below**********/
+// }
+
+// lifeInWeeks(21);
