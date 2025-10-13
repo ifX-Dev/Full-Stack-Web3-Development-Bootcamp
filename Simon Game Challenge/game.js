95c009f15c8238a7471c0abfc9978ff0771ca553
ifX-Dev <ifxdev03@gmail.com>
2025-10-13 20:57:35 +0100

    Built a game using jQuery

--- Simon Game Challenge/game.js
+++ Simon Game Challenge/game.js
@@ -2,8 +2,26 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
+var started;
+
+var level = 0;
+
+$("html").keydown(function () {
+  started = true;
+
+  $("h1").text("Level 0");
+
+  if (started == true) {
+    nextSequence();
+  } else {
+    console.log(started);
+  }
+});
+
 function nextSequence() {
-  var randomNumber = Math.floor(Math.random() * 3 + 1);
+  var randomNumber = Math.floor(Math.random() * 4);
 
   var randomChosenColour = buttonColours[randomNumber];
 
@@ -14,6 +32,59 @@ function nextSequence() {
     .fadeOut(100)
     .fadeIn(100);
 
-  var audio = new Audio("./sounds/" + randomChosenColour + ".mp3");
+  playSound(randomChosenColour);
+
+  level++;
+
+  $("h1").text("Level " + level);
+}
+
+function playSound(name) {
+  var audio = new Audio("./sounds/" + name + ".mp3");
   audio.play();
 }
+
+function animatePress(currentColour) {
+  $("#" + currentColour).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColour).removeClass("pressed");
+  }, 100);
+}
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  userClickedPattern = [];
+  started = false;
+}
+
+function checkAnswer(currentLevel) {
+  if (userClickedPattern[currentLevel] == gamePattern[currentLevel]) {
+    console.log("success");
+
+    if (userClickedPattern.length == gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+      userClickedPattern = [];
+    }
+  } else {
+    console.log("wrong");
+    $("h1").text("Game Over, Press Any Key to Restart");
+    $("body").addClass("game-over");
+    setTimeout(function () {
+      $("body").removeClass("game-over");
+    }, 200);
+    startOver();
+  }
+}
+
+$(".btn").click(function () {
+  var userChosenColour = this.id;
+  animatePress(userChosenColour);
+  playSound(userChosenColour);
+  userClickedPattern.push(userChosenColour);
+  console.log(userClickedPattern);
+  checkAnswer(userClickedPattern.length - 1);
+});
